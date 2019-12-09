c634f784a5d0daae2290bf88619999e4e1373025
Massiel50 <massielescano@yahoo.com>
2019-12-09 15:12:14 -0500

    commented notes for direction i will take

---
+++ Develop/script.js
@@ -0,0 +1,26 @@
+// 3 things
+
+// input for time slot
+// containers/ rows with text area for each hoir of the day
+$()
+
+//  save button saves input to local storage
+
+// get local storage when page is refreshed
+
+// compare present time for color coded pas present and future
+
+
+
+$(".saveBtn").on("click", function(event){
+
+})
+
+
+$("currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+// get hour from current time usig moemnt and compare to rows
+
+// use this for time and date= moment().format('MMMM Do YYYY, h:mm:ss a');
+
+// click button and get value of text area
