b5ece74b3a2ddd6d22d3a31e26671f64f388c15b
Massiel50 <massielescano@yahoo.com>
2019-12-10 20:08:35 -0500

    changed time format, button saves to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,26 @@
 // 3 things
 
-// display time and date 
-$("#currentDay").text(moment().format('MMMM Do YYYY'));
+// display time and date
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 // input for time slot
-
 // containers/ rows with text area for each hour of the day
 
-//  save button saves input to local storage
-var textInput = $("#textarea").val().trim();
+
+
+
+
+// click save button to save textInput to local storage 
+$(".saveBtn").on("click", function(event) {
+  event.preventDefault();
+  //  save button saves input to local storage
+  var textInput = $("#textarea").val().trim();
+  // set to local storage
+  localStorage.setItem(".hour", textInput);
+});
 // get local storage when page is refreshed
 
+// get hour from current time usig moemnt and compare to rows
 // compare present time for color coded pas present and future
 
 
-
-// $(".saveBtn").on("click", function(event){
-
-// })
-
-
-
-
-// get hour from current time usig moemnt and compare to rows
-
-// use this for time and date= moment().format('MMMM Do YYYY, h:mm:ss a');
-
-// click button and get value of text area
